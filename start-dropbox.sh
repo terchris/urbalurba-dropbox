fa3f9aba445deb5ccc12bec107b1bd0347a493bd
Terje Christensen <terchris@users.noreply.github.com>
2023-08-08 23:23:43 +0200

    test

--- start-dropbox.sh
+++ start-dropbox.sh
@@ -3,8 +3,8 @@
 # Prepare Dropbox directory
 mkdir -p $DROPBOX_DIR
 
-# Start Dropbox as root
-dropbox start
+# Start Dropbox as root to install the daemon
+dropbox start -i
 
 # Enable LAN sync
 sleep 10
